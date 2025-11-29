33f2ed30362a033f38dbe9e083b043c8668c87a0
David Isaksson <davidisaksson93@gmail.com>
2025-11-29 19:52:09 +0100

    Cleanup

--- bfception/generate_brainfuck_code.py
+++ bfception/generate_brainfuck_code.py
@@ -1,28 +1,32 @@
 #!/usr/bin/env python3
 
 import argparse
+from pathlib import Path
 
 from text_to_brainfuck.brainfuck_generator import string_to_bf
-
-BF_CODE_WIDTH = 120
 
 
 def _parse_args():
     parser = argparse.ArgumentParser("Generate Brainfuck code that prints C++ code")
-    parser.add_argument("input", type=str, help="Input C++ file to read")
+    parser.add_argument("input", type=Path, help="Input C++ file to read")
     parser.add_argument(
-        "--output", "-o", type=str, required=True, help="Output C++ file to write"
+        "--output", "-o", type=Path, required=True, help="Output C++ file to write"
     )
     return parser.parse_args()
 
 
+def _split_to_lines(s, width=220):
+    return [s[y - width : y] for y in range(width, len(s) + width, width)]
+
+
+def _format_c_append(s):
+    return f's+="{s}";\n'
+
+
 def _main():
     args = _parse_args()
-    main_cpp = None
-    with open(args.input, "r") as fd:
-        main_cpp = fd.read()
     split_marker = "// BF_HERE\n"
-    part_1, part_2 = main_cpp.split(split_marker, maxsplit=1)
+    part_1, part_2 = args.input.read_text().split(split_marker, maxsplit=1)
 
     # This is now Brainfuck code that prints the C++ code
     part_1_bf = string_to_bf(part_1, False)
@@ -31,31 +35,20 @@ def _main():
 
     # `mid` is what we want BF to print out in the end
     mid = part_1_bf + split_marker_bf + part_2_bf
-    mid_bf_lines = [
-        mid[y - BF_CODE_WIDTH : y]
-        for y in range(BF_CODE_WIDTH, len(mid) + BF_CODE_WIDTH, BF_CODE_WIDTH)
-    ]
-
-    def format_code_string(s):
-        return f's+="{s}";\n'
 
     # Now add this brainfuck code as many lines of C++ code
     mid_bf = ""
-    for bf_line in mid_bf_lines:
-        mid_bf += format_code_string(bf_line)
+    for line in _split_to_lines(mid):
+        mid_bf += _format_c_append(line)
+
     mid_bf_bf = string_to_bf(mid_bf, False)
 
-    main_stage_2 = part_1_bf + mid_bf_bf + part_2_bf
-    bf_results = [
-        main_stage_2[y - BF_CODE_WIDTH : y]
-        for y in range(BF_CODE_WIDTH, len(main_stage_2) + BF_CODE_WIDTH, BF_CODE_WIDTH)
-    ]
+    bf_results_bf = part_1
+    for line in _split_to_lines(part_1_bf + mid_bf_bf + part_2_bf):
+        bf_results_bf += _format_c_append(line)
+    bf_results_bf += part_2
 
-    with open(args.output, "w") as fd:
-        fd.write(part_1)
-        for bf_line in bf_results:
-            fd.write(format_code_string(bf_line))
-        fd.write(part_2)
+    args.output.write_text(bf_results_bf)
 
 
 if __name__ == "__main__":
